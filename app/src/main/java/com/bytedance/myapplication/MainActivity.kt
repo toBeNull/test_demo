0823e869531d61dc1fc8309236282b71e4df8862
luoqiaoyou <luoqiaoyou@bytedance.com>
2021-03-15 21:37:08 +0800

    first commit

---
+++ app/src/main/java/com/bytedance/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bytedance.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
